5038a469511f9d2c5e1ad5bad1822285e02f3732
MateBook <examletru@gmail.com>
2022-03-23 16:57:29 +0300

    first commit

--- src/pages/cabinet/profile.vue
+++ src/pages/cabinet/profile.vue
@@ -10,7 +10,7 @@
         </template>
 
         <div
-            class="cabinet__content p-20px h-full grid grid-rows-[1fr_2fr] grid-cols-[1fr_auto] gap-20px"
+            class="cabinet__content p-20px h-full grid grid-rows-[1fr_3fr] grid-cols-[1fr_auto] gap-20px"
         >
             <div class="bg-[#fff] border-1px border-[#e1e3e6] rounded p2 row-span-2 col-start-2">
                 <div class="grid gap-10px">
@@ -35,7 +35,7 @@
                         placeholder="a1kk3@gmail.com"
                         class="h-30px w-full md:col-span-full lg:col-span-1"
                     />
-                    <QButton class="h-40px mt-10px">Сохранить</QButton>
+                    <QButton class="h-40px mt-10px">Обновить</QButton>
                 </div>
             </div>
 
