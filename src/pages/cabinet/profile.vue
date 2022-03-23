4cd910c2cfa9cc6ce58fa1a4266fe195bcca554e
MateBook <examletru@gmail.com>
2022-03-23 17:31:43 +0300

    first commit

--- src/pages/cabinet/profile.vue
+++ src/pages/cabinet/profile.vue
@@ -1,4 +1,6 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const photo_url = "https://sun9-18.userapi.com/s/v1/ig2/ZNUHd8FflAqjWD5ZaCgDp1vPNmwUYX3e9b7QUn4YBurMUCmXTY7oyFNEdU2OaDmS8oKd0Nt2FyN8qnS3F0FVExSG.jpg?size=200x200&quality=95&crop=0,0,900,900&ava=1"
+</script>
 
 <template>
     <NuxtLayout name="cabinet">
@@ -13,24 +15,25 @@
             class="cabinet__content p-20px h-full grid grid-rows-[1fr_3fr] grid-cols-[1fr_auto] gap-20px"
         >
             <div class="bg-[#fff] border-1px border-[#e1e3e6] rounded p2 row-span-2 col-start-2">
-                <div class="grid gap-10px">
-                    <div>Фамилия</div>
-                    <QInput placeholder="Лукин" class="h-30px w-full" />
-                    <div>Имя</div>
-                    <QInput placeholder="Вадим" class="h-30px w-full" />
-                    <div>Отчество</div>
-                    <QInput placeholder="Олегович" class="h-30px w-full" />
-                    <div>Логин</div>
+                <div class="grid gap-10px p-10px">
+                    <div class="h-72px w-72px mx-auto">
+                        <img class="rounded-50px" :src="photo_url" />
+                    </div>
+                    <div class="text-sm">Имя</div>
+                    <QInput placeholder="Вадим Лукин" class="h-30px w-full" />
+                    <div class="text-sm">Логин</div>
                     <QInput placeholder="a1kk3" class="h-30px w-full" />
-                    <div>Пароль</div>
+                    <div class="text-sm">Пароль</div>
                     <QInput placeholder="**********" class="h-30px w-full" />
-                    <div>Почта</div>
+                    <div class="text-sm">Почта</div>
                     <QInput placeholder="a1kk3@mail.ru" class="h-30px w-full" />
-                    <div>ВКонтакте</div>
+                    <div class="text-sm">Телефон</div>
+                    <QInput placeholder="+79879999999" class="h-30px w-full" />
+                    <div class="text-sm">ВКонтакте</div>
                     <QInput placeholder="vk.com/a1kk3" class="h-30px w-full" />
-                    <div>Telegram</div>
+                    <div class="text-sm">Telegram</div>
                     <QInput placeholder="@a1kk3" class="h-30px w-full" />
-                    <div>Google</div>
+                    <div class="text-sm">Google</div>
                     <QInput
                         placeholder="a1kk3@gmail.com"
                         class="h-30px w-full md:col-span-full lg:col-span-1"
